de3caa8e4b09bf7c2bc78b46f0ee835861fdd69a
Vilius Roškus <vro@danskebank.lt>
2024-03-06 14:24:00 +0200

    Update

--- bin/index.js
+++ bin/index.js
@@ -5,21 +5,52 @@ const {
 } = require('commander');
 const {
   bundle,
-} = require('../dist/index.js');
+} = require('../dist');
 
 const packageJson = require('../package.json');
 
 program
   .usage('[OPTIONS]...')
-  .option('-t, --types <flow | tsc>', 'define static type to use', 'tsc')
-  .option('-i, --input-file <path>', 'input file path', 'src/index.js')
-  .option('-x, --index-html-dir <path>', 'index.html file dir path', 'public')
-  .option('-o, --output-dir <path>', 'output dir path', 'dist')
-  .option('-m, --sourcemap', 'generate source map files')
-  .option('-c, --coverage', 'instruct code to collect coverage data')
-  .option('-s, --serve <port>', 'start development server on port')
-  .option('-d, --debug', 'debug build process')
-  .version(packageJson.version, '-v, --version')
+  .option(
+    '-t, --types <flow | tsc>',
+    'define static type to use',
+    'tsc',
+  )
+  .option(
+    '-i, --input-file <path>',
+    'input file path',
+    'src/index.js',
+  )
+  .option(
+    '-x, --index-html-dir <path>',
+    'index.html file dir path',
+    'public',
+  )
+  .option(
+    '-o, --output-dir <path>',
+    'output dir path',
+    'dist',
+  )
+  .option(
+    '-m, --sourcemap',
+    'generate source map files',
+  )
+  .option(
+    '-c, --coverage',
+    'instruct code to collect coverage data',
+  )
+  .option(
+    '-s, --serve <port>',
+    'start development server on port',
+  )
+  .option(
+    '-d, --debug',
+    'debug build process',
+  )
+  .version(
+    packageJson.version,
+    '-v, --version',
+  )
   .parse(process.argv);
 
 const options = program.opts();
--- dist/index.js
+++ dist/index.js
@@ -1,313 +1,291 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-const esbuild = require('esbuild');
-const sassPlugin = require('esbuild-plugin-sass');
-const flowPlugin = require('esbuild-plugin-flow');
-const tscPlugin = require('esbuild-plugin-tsc');
-const {
-  esbuildPluginIstanbul,
-} = require('esbuild-plugin-istanbul');
-const browserslist = require('browserslist');
-const {
-  resolveToEsbuildTarget,
-} = require('esbuild-plugin-browserslist');
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
 
-const preparedSettings = ({
+// src/index.js
+var import_http = __toESM(require("http"));
+var import_path = __toESM(require("path"));
+var import_fs = __toESM(require("fs"));
+var import_esbuild = __toESM(require("esbuild"));
+var import_esbuild_plugin_sass = __toESM(require("esbuild-plugin-sass"));
+var import_esbuild_plugin_flow = __toESM(require("esbuild-plugin-flow"));
+var import_esbuild_plugin_tsc = __toESM(require("esbuild-plugin-tsc"));
+var import_esbuild_plugin_istanbul = require("esbuild-plugin-istanbul");
+var import_browserslist = __toESM(require("browserslist"));
+var import_esbuild_plugin_browserslist = require("esbuild-plugin-browserslist");
+var preparedSettings = ({
   coverage,
   customPlugins,
   inputFilePath,
   outputDirPath,
   sourcemap,
-  types,
+  types
 }) => {
   const plugins = [
-    types === 'flow' ? flowPlugin(/\.js$|\.jsx$/) : tscPlugin(),
-    sassPlugin(),
+    types === "flow" ? (0, import_esbuild_plugin_flow.default)(/\.js$|\.jsx$/) : (0, import_esbuild_plugin_tsc.default)(),
+    (0, import_esbuild_plugin_sass.default)()
   ];
-
   if (Array.isArray(customPlugins)) {
     customPlugins.forEach((customPlugin) => {
       plugins.push(customPlugin);
     });
   }
-
   if (coverage) {
-    plugins.push(esbuildPluginIstanbul({
+    plugins.push((0, import_esbuild_plugin_istanbul.esbuildPluginIstanbul)({
       filter: /\.[cm]?js$/,
-      loader: 'js',
-      name: 'istanbul-loader-js',
+      loader: "js",
+      name: "istanbul-loader-js"
     }));
-
-    plugins.push(esbuildPluginIstanbul({
+    plugins.push((0, import_esbuild_plugin_istanbul.esbuildPluginIstanbul)({
       filter: /\.jsx$/,
-      loader: 'jsx',
-      name: 'istanbul-loader-jsx',
+      loader: "jsx",
+      name: "istanbul-loader-jsx"
     }));
-
-    plugins.push(esbuildPluginIstanbul({
+    plugins.push((0, import_esbuild_plugin_istanbul.esbuildPluginIstanbul)({
       filter: /\.[cm]?ts$/,
-      loader: 'ts',
-      name: 'istanbul-loader-ts',
+      loader: "ts",
+      name: "istanbul-loader-ts"
     }));
-
-    plugins.push(esbuildPluginIstanbul({
+    plugins.push((0, import_esbuild_plugin_istanbul.esbuildPluginIstanbul)({
       filter: /\.tsx$/,
-      loader: 'tsx',
-      name: 'istanbul-loader-tsx',
+      loader: "tsx",
+      name: "istanbul-loader-tsx"
     }));
   }
-
   return {
     bundle: true,
-    entryPoints: [path.join(
+    entryPoints: [import_path.default.join(
       process.cwd(),
-      inputFilePath,
+      inputFilePath
     )],
     loader: {
-      '.eot': 'dataurl',
-      '.js': 'jsx',
-      '.png': 'dataurl',
-      '.svg': 'dataurl',
-      '.ttf': 'dataurl',
-      '.woff': 'dataurl',
-      '.woff2': 'dataurl',
+      ".eot": "dataurl",
+      ".js": "jsx",
+      ".png": "dataurl",
+      ".svg": "dataurl",
+      ".ttf": "dataurl",
+      ".woff": "dataurl",
+      ".woff2": "dataurl"
     },
     minify: true,
-    outfile: path.join(
+    outfile: import_path.default.join(
       process.cwd(),
-      `${outputDirPath}/index.js`,
+      `${outputDirPath}/index.js`
     ),
     plugins,
     sourcemap,
-    target: resolveToEsbuildTarget(browserslist(), {
-      printUnknownTargets: false,
-    }),
+    target: (0, import_esbuild_plugin_browserslist.resolveToEsbuildTarget)((0, import_browserslist.default)(), {
+      printUnknownTargets: false
+    })
   };
 };
-
-const build = async ({
-  settings,
+var build = async ({
+  settings
 }) => {
-  await esbuild.build(settings);
+  await import_esbuild.default.build(settings);
 };
-
-const copyFiles = async ({
+var copyFiles = async ({
   indexHtmlDirPath,
-  outputDirPath,
+  outputDirPath
 }) => {
-  fs.cpSync(
-    path.join(
+  import_fs.default.cpSync(
+    import_path.default.join(
       process.cwd(),
-      indexHtmlDirPath,
+      indexHtmlDirPath
     ),
-    path.join(
+    import_path.default.join(
       process.cwd(),
-      outputDirPath,
+      outputDirPath
     ),
     {
-      recursive: true,
-    },
+      recursive: true
+    }
   );
-
-  const indexHtmlFilePath = path.join(
+  const indexHtmlFilePath = import_path.default.join(
     process.cwd(),
-    `${outputDirPath}/index.html`,
+    `${outputDirPath}/index.html`
   );
-
-  let indexHtmlFile = fs.readFileSync(
+  let indexHtmlFile = import_fs.default.readFileSync(
     indexHtmlFilePath,
     {
-      encoding: 'utf8',
-      flag: 'r',
-    },
+      encoding: "utf8",
+      flag: "r"
+    }
   );
-
   indexHtmlFile = indexHtmlFile.replace(
     /\?ts/g,
-    `?${new Date().getTime()}`,
+    `?${(/* @__PURE__ */ new Date()).getTime()}`
   );
-
-  fs.writeFileSync(
+  import_fs.default.writeFileSync(
     indexHtmlFilePath,
     indexHtmlFile,
     {
-      encoding: 'utf8',
-    },
+      encoding: "utf8"
+    }
   );
 };
-
-const serve = async ({
+var serve = async ({
   outputDirPath,
   servePort,
-  settings,
+  settings
 }) => {
-  const packageName = process.env.npm_package_name || 'Unknown package';
-  const packageVersion = process.env.npm_package_version || 'Unknown version';
-
-  const ctx = await esbuild.context({
+  const packageName = process.env.npm_package_name || "Unknown package";
+  const packageVersion = process.env.npm_package_version || "Unknown version";
+  const ctx = await import_esbuild.default.context({
     ...settings,
     minify: false,
     plugins: [
       ...settings.plugins,
       {
-        name: 'watch',
+        name: "watch",
         setup(b) {
           let start = 0;
-
           b.onStart(() => {
             start = process.hrtime();
-            console.log('Starting build...');
+            console.log("Starting build...");
           });
-
           b.onEnd(() => {
             const end = process.hrtime(start);
-
-            const duration = (end[0] * 1000000000 + end[1]) / 1000000000;
-
+            const duration = (end[0] * 1e9 + end[1]) / 1e9;
             console.log(
-              `Built in: ${duration.toFixed(2)}s`,
+              `Built in: ${duration.toFixed(2)}s`
             );
           });
-        },
-      },
+        }
+      }
     ],
-    sourcemap: false,
+    sourcemap: false
   });
-
   const innerServer = await ctx.serve({
-    servedir: outputDirPath,
+    servedir: outputDirPath
   });
-
-  http
-    .createServer((req, res) => {
-      const options = {
-        headers: req.headers,
-        hostname: innerServer.host,
-        method: req.method,
-        path: req.url,
-        port: innerServer.port,
-      };
-
-      const proxyReq = http.request(
-        options,
-        (proxyRes) => {
-          if (proxyRes.statusCode === 404) {
-            const index = fs.createReadStream(`${outputDirPath}/index.html`);
-
-            return index.pipe(res);
+  import_http.default.createServer((req, res) => {
+    const options = {
+      headers: req.headers,
+      hostname: innerServer.host,
+      method: req.method,
+      path: req.url,
+      port: innerServer.port
+    };
+    const proxyReq = import_http.default.request(
+      options,
+      (proxyRes) => {
+        if (proxyRes.statusCode === 404) {
+          const index = import_fs.default.createReadStream(`${outputDirPath}/index.html`);
+          return index.pipe(res);
+        }
+        res.writeHead(
+          proxyRes.statusCode,
+          proxyRes.headers
+        );
+        proxyRes.pipe(
+          res,
+          {
+            end: true
           }
-
-          res.writeHead(
-            proxyRes.statusCode,
-            proxyRes.headers,
-          );
-          proxyRes.pipe(
-            res,
-            {
-              end: true,
-            },
-          );
-
-          return null;
-        },
-      );
-
-      req.pipe(
-        proxyReq,
-        {
-          end: true,
-        },
-      );
-    })
-    .listen(
-      Number(servePort),
-      () => console.info(`${packageName}:${packageVersion} is listening on ${servePort}`),
+        );
+        return null;
+      }
     );
+    req.pipe(
+      proxyReq,
+      {
+        end: true
+      }
+    );
+  }).listen(
+    Number(servePort),
+    () => console.info(`${packageName}:${packageVersion} is listening on ${servePort}`)
+  );
 };
-
-const getConfigValue = (
-  configParam,
-  envParam,
-  defaultValue,
-) => configParam || process.env[envParam] || defaultValue;
-
-const bundle = async (config) => {
+var getConfigValue = (configParam, envParam, defaultValue) => configParam || process.env[envParam] || defaultValue;
+var bundle = async (config) => {
   const {
     customPlugins,
-    debug,
+    debug
   } = config;
-
   const coverage = getConfigValue(
     config.coverage,
-    'COVERAGE',
-    false,
+    "COVERAGE",
+    false
   );
   const indexHtmlDirPath = getConfigValue(
     config.indexHtmlDirPath,
-    'INDEX_PATH',
-    'public',
+    "INDEX_PATH",
+    "public"
   );
   const inputFilePath = getConfigValue(
     config.inputFilePath,
-    'SOURCE_FILE_PATH',
-    'src/index.js',
+    "SOURCE_FILE_PATH",
+    "src/index.js"
   );
   const outputDirPath = getConfigValue(
     config.outputDirPath,
-    'BUILD_PATH',
-    'dist',
+    "BUILD_PATH",
+    "dist"
   );
   const servePort = getConfigValue(
     config.servePort,
-    'SERVE',
-    0,
+    "SERVE",
+    0
   );
   const sourcemap = getConfigValue(
     config.sourcemap,
-    'SOURCEMAP',
-    true,
+    "SOURCEMAP",
+    true
   );
   const types = getConfigValue(
     config.types,
-    'TYPES',
-    'tsc',
+    "TYPES",
+    "tsc"
   );
-
   const settings = preparedSettings({
     coverage,
     customPlugins,
     inputFilePath,
     outputDirPath,
     sourcemap,
-    types,
+    types
   });
-
   if (debug) {
-    console.log('build config: ', config);
-    console.log('build settings: ', settings);
+    console.log("build config: ", config);
+    console.log("build settings: ", settings);
   }
-
   await copyFiles({
     indexHtmlDirPath,
-    outputDirPath,
+    outputDirPath
   });
-
   if (servePort) {
     await serve({
       outputDirPath,
       servePort,
-      settings,
+      settings
     });
   } else {
     await build({
-      settings,
+      settings
     });
   }
 };
-
 module.exports = {
-  bundle,
+  bundle
 };
-
-
